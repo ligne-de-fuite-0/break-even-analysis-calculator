ba31564125177b0ab9ca91b3abbd4b475b2d1b96
ligne-de-fuite-0 <111183912+ligne-de-fuite-0@users.noreply.github.com>
2025-07-02 14:57:11 +0800

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export const CalculatorIcon: React.FC<{ className?: string }> = ({ className }) => (
@@ -38,5 +37,13 @@ export const PlusIcon: React.FC<{ className?: string }> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
         <line x1="12" y1="5" x2="12" y2="19"></line>
         <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+);
+
+export const DownloadIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+        <polyline points="7 10 12 15 17 10"></polyline>
+        <line x1="12" y1="15" x2="12" y2="3"></line>
     </svg>
 );
